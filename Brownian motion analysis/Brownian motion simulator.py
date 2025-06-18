73be8a7a24e625d6b7e559ff8c0a0dae47f110cc
Robin <116107268+Robin1853@users.noreply.github.com>
2025-06-18 17:03:38 +0200

    Add files via upload

--- Brownian motion analysis/Brownian motion simulator.py
+++ Brownian motion analysis/Brownian motion simulator.py
@@ -21,7 +21,7 @@ drift_test_point = (2,2,2)          #Point of lokal drift test
 
 ###lowpass filter denoising approach parameters
 radius = 0.27 #0.27                       #radius of local trajectories for drift estimation
-ab_ratio = 0.012 #0.01241 -0.01242                     #Sets the alpha/beta ratio of the denoising loss function: alpha * d + beta * c
+ab_ratio = 1.0                     #Sets the alpha/beta ratio of the denoising loss function: alpha * d + beta * c
                                     # where d(ifference) is the difference between the denoised trajectory and original and c(urvature) is the roughness of the denoised trajectory
 ###local denoising approach  parameters
 radii_ratio = 1                     #factor for evaluation distance around grid points (should be ten times grid_step)
@@ -43,16 +43,16 @@ np.random.seed(0)
 #Create Trjectories and original drift field
 signal = trajectory(drift_func, D, dim, dt, N, M)
 true_drifts, x_drift, y_drift, z_drift = drift_dynamics(drift_func, grid_step, plot_range)
-print(x_drift[drift_test_point], y_drift[drift_test_point], z_drift[drift_test_point])
+# print(x_drift[drift_test_point], y_drift[drift_test_point], z_drift[drift_test_point])
 
 #####Unsupervised approach estimating local drift from denoised trajectories (lowpass filter)
 ###Estimating local diffusion from reverse filter
-#lfa.plot_welch_spectra(signal[0], dt)
-#lfa.local_filtered_estimator(signal, true_drifts, grid_step, radius, ab_ratio, dt)
+lfa.plot_welch_spectra(signal[0], dt)
+lfa.local_filtered_estimator(signal, true_drifts, grid_step, radius, ab_ratio, dt)
 
 ####Unsupervised approach estimating local drift from local gaussian weighted movement
-est_drift, U, V, W =lda.local_drift_estimator(signal, dt = dt, space_borders=plot_range, grid_step=grid_step, radii_ratio=radii_ratio)
-print([np.mean(U), np.mean(V), np.mean(W)], lda.score(true_drifts, est_drift))
-
+est_drift, U, V, W =lda.local_estimator(signal, dt = dt, space_borders=plot_range, grid_step=grid_step, radii_ratio=radii_ratio)
+lda.score(true_drifts, est_drift)
+#print(U[drift_test_point], V[drift_test_point], W[drift_test_point])
 
 plt.show()
